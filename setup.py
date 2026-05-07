7e824b6892bac0bd244b031302398237f2e6348f
ragibcs <hasan2305101775@diu.edu.bd>
2026-05-08 00:49:16 +0600

    setup.py file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medibot",
+    version="0.1",
+    author="Ragib Hasan Mondal",
+    packages=find_packages(),
+    install_requires=[],
+)
